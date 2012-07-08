f0243b2abcf5374cf139c050dab263075e09b33a
Dante Francesco Passera <dante.francesco.passera@gmail.com>
2012-07-08 15:29:43 -0400

    added fpo images to rough panels

--- ATU_hiscore/gen/com/smashingboxes/ATUHiscore/R.java
+++ ATU_hiscore/gen/com/smashingboxes/ATUHiscore/R.java
@@ -26,41 +26,46 @@ public final class R {
         public static final int edit_text_error=0x7f020001;
         public static final int edit_text_good=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int instructions_panel=0x7f020004;
-        public static final int title_box=0x7f020005;
-        public static final int title_box_short=0x7f020006;
+        public static final int instructions_fpo=0x7f020004;
+        public static final int instructions_panel=0x7f020005;
+        public static final int title_box=0x7f020006;
+        public static final int title_box_short=0x7f020007;
     }
     public static final class id {
-        public static final int button_register=0x7f07000e;
-        public static final int button_signIn=0x7f07000f;
+        public static final int button_register=0x7f070012;
+        public static final int button_signIn=0x7f070013;
         public static final int home_title=0x7f070000;
         public static final int horizontal_1=0x7f070003;
         public static final int instructions_header1=0x7f070002;
         public static final int instructions_header2=0x7f070004;
+        public static final int instructions_image1=0x7f070006;
+        public static final int instructions_image2=0x7f070009;
+        public static final int instructions_image3=0x7f07000a;
+        public static final int instructions_image4=0x7f07000d;
         public static final int instructions_panel1=0x7f070005;
-        public static final int instructions_panel2=0x7f070007;
-        public static final int instructions_panel3=0x7f070009;
-        public static final int instructions_panelText1=0x7f070006;
-        public static final int instructions_panelText2=0x7f070008;
-        public static final int instructions_panelText3=0x7f07000a;
+        public static final int instructions_panel2=0x7f070008;
+        public static final int instructions_panel3=0x7f07000c;
+        public static final int instructions_panelText1=0x7f070007;
+        public static final int instructions_panelText2=0x7f07000b;
+        public static final int instructions_panelText3=0x7f07000e;
         public static final int instructions_title=0x7f070001;
-        public static final int main_blurb=0x7f07000d;
-        public static final int main_bySB=0x7f07000c;
-        public static final int main_title=0x7f07000b;
-        public static final int register_button_cancel=0x7f07001d;
-        public static final int register_button_submit=0x7f07001c;
-        public static final int register_field_email=0x7f070015;
-        public static final int register_field_email_confirm=0x7f070017;
-        public static final int register_field_name=0x7f070013;
-        public static final int register_field_pin=0x7f070019;
-        public static final int register_header=0x7f070011;
-        public static final int register_label_email=0x7f070014;
-        public static final int register_label_email_confirm=0x7f070016;
-        public static final int register_label_name=0x7f070012;
-        public static final int register_label_pin=0x7f070018;
-        public static final int register_opt_in1=0x7f07001a;
-        public static final int register_opt_in2=0x7f07001b;
-        public static final int register_title=0x7f070010;
+        public static final int main_blurb=0x7f070011;
+        public static final int main_bySB=0x7f070010;
+        public static final int main_title=0x7f07000f;
+        public static final int register_button_cancel=0x7f070021;
+        public static final int register_button_submit=0x7f070020;
+        public static final int register_field_email=0x7f070019;
+        public static final int register_field_email_confirm=0x7f07001b;
+        public static final int register_field_name=0x7f070017;
+        public static final int register_field_pin=0x7f07001d;
+        public static final int register_header=0x7f070015;
+        public static final int register_label_email=0x7f070018;
+        public static final int register_label_email_confirm=0x7f07001a;
+        public static final int register_label_name=0x7f070016;
+        public static final int register_label_pin=0x7f07001c;
+        public static final int register_opt_in1=0x7f07001e;
+        public static final int register_opt_in2=0x7f07001f;
+        public static final int register_title=0x7f070014;
     }
     public static final class layout {
         public static final int home_layout=0x7f030000;
@@ -70,34 +75,38 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int button_cancel=0x7f050005;
-        public static final int button_confirm=0x7f050003;
+        public static final int button_cancel=0x7f050007;
+        public static final int button_confirm=0x7f050005;
         /**   general labels 
          */
-        public static final int button_register=0x7f050001;
-        public static final int button_signIn=0x7f050002;
-        public static final int button_submit=0x7f050004;
-        public static final int instructions_panel1=0x7f050012;
-        public static final int instructions_panel2=0x7f050013;
-        public static final int instructions_panel3=0x7f050014;
+        public static final int button_register=0x7f050003;
+        public static final int button_signIn=0x7f050004;
+        public static final int button_submit=0x7f050006;
+        public static final int instructions_panel1=0x7f050014;
+        public static final int instructions_panel2=0x7f050015;
+        public static final int instructions_panel3=0x7f050016;
         /**  instructions 
          */
-        public static final int instructions_regConfirm1=0x7f050010;
-        public static final int instructions_regConfirm2=0x7f050011;
-        public static final int main_blurb=0x7f050007;
-        public static final int main_bySB=0x7f050008;
+        public static final int instructions_regConfirm1=0x7f050012;
+        public static final int instructions_regConfirm2=0x7f050013;
+        public static final int main_blurb=0x7f050009;
+        public static final int main_bySB=0x7f05000a;
         /**  main 
          */
-        public static final int main_title=0x7f050006;
+        public static final int main_title=0x7f050008;
         /**  register 
          */
-        public static final int register_header=0x7f050009;
-        public static final int register_label_email=0x7f05000b;
-        public static final int register_label_email_confirm=0x7f05000c;
-        public static final int register_label_name=0x7f05000a;
-        public static final int register_label_pin=0x7f05000d;
-        public static final int register_opt_in1=0x7f05000e;
-        public static final int register_opt_in2=0x7f05000f;
+        public static final int register_header=0x7f05000b;
+        public static final int register_label_email=0x7f05000d;
+        public static final int register_label_email_confirm=0x7f05000e;
+        public static final int register_label_name=0x7f05000c;
+        public static final int register_label_pin=0x7f05000f;
+        public static final int register_opt_in1=0x7f050010;
+        public static final int register_opt_in2=0x7f050011;
+        /**   general measurements 
+         */
+        public static final int standard_margin=0x7f050001;
+        public static final int standard_padding=0x7f050002;
     }
     public static final class style {
         public static final int instructions_panel=0x7f060002;
